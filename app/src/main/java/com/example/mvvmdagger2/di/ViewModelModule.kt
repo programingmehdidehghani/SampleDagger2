be043fe4fcd89851492825f1a33f807dcc2b55a1
mehdi <dehghani_m@rocketmail.com>
2022-12-08 16:20:02 +0330

    make instance view model

--- app/src/main/java/com/example/mvvmdagger2/di/ViewModelModule.kt
+++ app/src/main/java/com/example/mvvmdagger2/di/ViewModelModule.kt
@@ -5,14 +5,20 @@ import com.example.mvvmdagger2.viewModels.MainViewModel
 import com.example.mvvmdagger2.viewModels.MainViewModel2
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import dagger.multibindings.StringKey
 
 
 @Module
 abstract class ViewModelModule {
 
     @Binds
+    @StringKey("mainViewModel")
+    @IntoMap
     abstract fun mainViewModel(mainViewModel: MainViewModel) : ViewModel
 
     @Binds
+    @StringKey("mainViewModel2")
+    @IntoMap
     abstract fun mainViewModel2(mainViewModel2: MainViewModel2) : ViewModel
 }
