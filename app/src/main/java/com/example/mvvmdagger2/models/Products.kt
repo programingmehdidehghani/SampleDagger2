9b47b7f5ec6b6d2621796ac12a3a9099b446e209
mehdi <dehghani_m@rocketmail.com>
2022-12-08 11:31:02 +0330

    delete extra jsons

--- app/src/main/java/com/example/mvvmdagger2/models/Products.kt
+++ app/src/main/java/com/example/mvvmdagger2/models/Products.kt
@@ -1,3 +1,10 @@
 package com.example.mvvmdagger2.models
 
-class Products : ArrayList<ProductsItem>()
+data class Products(
+    val category: String,
+    val description: String,
+    val id: Int,
+    val image: String,
+    val price: Double,
+    val title: String
+)
--- app/src/main/java/com/example/mvvmdagger2/models/Rating.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mvvmdagger2.models
-
-data class Rating(
-    val count: Int,
-    val rate: Double
-)
