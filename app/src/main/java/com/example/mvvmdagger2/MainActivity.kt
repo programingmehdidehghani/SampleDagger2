1640662d816dc0494f8359bee7ab1f512ab5042f
mehdi <dehghani_m@rocketmail.com>
2022-12-08 14:00:09 +0330

    make in activity

--- app/src/main/java/com/example/mvvmdagger2/MainActivity.kt
+++ app/src/main/java/com/example/mvvmdagger2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mvvmdagger2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmdagger2.viewModels.MainViewModel
 import com.example.mvvmdagger2.viewModels.MainViewModelFactory
@@ -14,6 +16,9 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var mainViewModelFactory: MainViewModelFactory
 
+    private val products : TextView
+    get() = findViewById(R.id.product)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,5 +26,9 @@ class MainActivity : AppCompatActivity() {
         (application as FakerApplication).applicationComponent.inject(this)
 
         mainViewModel = ViewModelProvider(this,mainViewModelFactory).get(MainViewModel::class.java)
+
+        mainViewModel.products.observe(this, Observer {
+            products.text = it.joinToString { x -> x.title + "\n\n" }
+        })
     }
 }
