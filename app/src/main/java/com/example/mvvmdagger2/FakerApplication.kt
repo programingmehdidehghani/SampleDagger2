5f7089033827b1941d6c5577ea39f656b92535ea
mehdi <dehghani_m@rocketmail.com>
2022-12-08 11:49:06 +0330

    make application class

---
+++ app/src/main/java/com/example/mvvmdagger2/FakerApplication.kt
@@ -0,0 +1,4 @@
+package com.example.mvvmdagger2
+
+class FakerApplication {
+}
--- app/src/main/java/com/example/mvvmdagger2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/mvvmdagger2/di/ApplicationComponent.kt
@@ -1,9 +1,12 @@
 package com.example.mvvmdagger2.di
 
+import com.example.mvvmdagger2.MainActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [NetworkModule::class])
 interface ApplicationComponent {
+
+    fun inject(mainActivity: MainActivity)
 }
