bf0318aa5301f20a9c44a83000e08451aaae2eeb
mehdi <dehghani_m@rocketmail.com>
2022-12-08 16:24:14 +0330

    continue instance view model

--- app/src/main/java/com/example/mvvmdagger2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/mvvmdagger2/di/ApplicationComponent.kt
@@ -1,16 +1,19 @@
 package com.example.mvvmdagger2.di
 
 import android.content.Context
+import androidx.lifecycle.ViewModel
 import com.example.mvvmdagger2.MainActivity
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [NetworkModule::class, DatabaseModule::class])
+@Component(modules = [NetworkModule::class, DatabaseModule::class, ViewModelModule::class])
 interface ApplicationComponent {
 
     fun inject(mainActivity: MainActivity)
+
+    fun getMap() : Map<String,ViewModel>
 
     @Component.Factory
     interface Factory{
