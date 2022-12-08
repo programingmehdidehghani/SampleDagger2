b029dfc3a2dc387e2028e7433d73a53bf5ea204e
mehdi <dehghani_m@rocketmail.com>
2022-12-08 13:54:17 +0330

    make instance application in ui

--- app/src/main/java/com/example/mvvmdagger2/MainActivity.kt
+++ app/src/main/java/com/example/mvvmdagger2/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.mvvmdagger2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvmdagger2.viewModels.MainViewModel
+import com.example.mvvmdagger2.viewModels.MainViewModelFactory
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var mainViewModelFactory: MainViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (application as FakerApplication).applicationComponent.inject(this)
+
+        mainViewModel = ViewModelProvider(this,mainViewModelFactory).get(MainViewModel::class.java)
     }
 }
