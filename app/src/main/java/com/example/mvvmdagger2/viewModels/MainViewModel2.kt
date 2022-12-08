6bc7909add0282c13790ad6b979278563e3a423e
mehdi <dehghani_m@rocketmail.com>
2022-12-08 16:09:17 +0330

    make view model 2

---
+++ app/src/main/java/com/example/mvvmdagger2/viewModels/MainViewModel2.kt
@@ -0,0 +1,11 @@
+package com.example.mvvmdagger2.viewModels
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class MainViewModel2 @Inject constructor(private val randomize: Randomize) : ViewModel() {
+
+    init {
+        randomize.doAction()
+    }
+}
--- app/src/main/java/com/example/mvvmdagger2/viewModels/MainViewModelFactory.kt
+++ app/src/main/java/com/example/mvvmdagger2/viewModels/MainViewModelFactory.kt
@@ -5,10 +5,9 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmdagger2.repository.ProductRepository
 import javax.inject.Inject
 
-class MainViewModelFactory @Inject constructor(private val repository: ProductRepository
-      , private val randomize: Randomize) : ViewModelProvider.Factory {
+class MainViewModelFactory @Inject constructor(private val mainViewModel: MainViewModel) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return MainViewModel(repository,randomize) as T
+        return mainViewModel as T
     }
 }
