db79210130820d4454d4b82f31884da1cf87b4e9
mehdi <dehghani_m@rocketmail.com>
2022-12-08 15:13:49 +0330

    add package db

--- app/src/main/java/com/example/mvvmdagger2/models/Product.kt
+++ app/src/main/java/com/example/mvvmdagger2/models/Product.kt
@@ -1,8 +1,13 @@
 package com.example.mvvmdagger2.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Product(
     val category: String,
     val description: String,
+    @PrimaryKey(autoGenerate = false)
     val id: Int,
     val image: String,
     val price: Double,
