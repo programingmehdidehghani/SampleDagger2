7bce9778b2b4f1bde414dbd7097427df19e0bc16
mehdi <dehghani_m@rocketmail.com>
2022-12-08 13:40:08 +0330

    start make repository

--- app/src/main/java/com/example/mvvmdagger2/repository/ProductRepository.kt
+++ app/src/main/java/com/example/mvvmdagger2/repository/ProductRepository.kt
@@ -1,8 +1,21 @@
 package com.example.mvvmdagger2.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.mvvmdagger2.models.Product
 import com.example.mvvmdagger2.retrofit.FakerApi
 import javax.inject.Inject
 
 class ProductRepository @Inject constructor(private val fakerApi: FakerApi) {
 
+    private val _products = MutableLiveData<List<Product>>()
+    val products : LiveData<List<Product>>
+    get() = _products
+
+    suspend fun getProducts(){
+        val result = fakerApi.getProducts()
+        if (result.isSuccessful && result.body() != null){
+            _products.postValue(result.body())
+        }
+    }
 }
