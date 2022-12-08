2318cf137091b5876d48ee1e8dd5ae0ae111bdd8
mehdi <dehghani_m@rocketmail.com>
2022-12-08 13:45:16 +0330

    get products in view model

--- app/src/main/java/com/example/mvvmdagger2/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmdagger2/viewModels/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.mvvmdagger2.viewModels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmdagger2.models.Product
 import com.example.mvvmdagger2.repository.ProductRepository
+import kotlinx.coroutines.launch
 
-class MainViewModel(val repository: ProductRepository) : ViewModel(){
+class MainViewModel(private val repository: ProductRepository) : ViewModel(){
+
+    val products : LiveData<List<Product>>
+    get() = repository.products
+
+    init {
+        viewModelScope.launch {
+            repository.getProducts()
+        }
+    }
 }
