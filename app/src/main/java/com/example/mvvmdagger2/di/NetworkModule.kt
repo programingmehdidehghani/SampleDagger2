249d90ff152029808471392e9a0c26a76b3e2b29
mehdi <dehghani_m@rocketmail.com>
2022-12-08 11:41:03 +0330

    make instance retrofit

---
+++ app/src/main/java/com/example/mvvmdagger2/di/NetworkModule.kt
@@ -0,0 +1,21 @@
+package com.example.mvvmdagger2.di
+
+import com.example.mvvmdagger2.utils.Constants
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun providesRetrofit() : Retrofit{
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
